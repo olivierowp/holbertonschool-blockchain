74667ad2ffcb2e14d9e44ed08a604da0c284c500
Olivier <odutartre@onewealthplace.com>
2020-11-27 19:26:22 +0100

    0x00 : Add . EC_KEY - Load from file

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -14,15 +14,16 @@
 #include <openssl/pem.h>
 #include <openssl/sha.h>
 
-#define EC_CURVE	NID_secp256k1
+#define EC_CURVE NID_secp256k1
 /* EC_KEY public key octet string length (using 256-bit curve) */
-#define EC_PUB_LEN	65
+#define EC_PUB_LEN 65
 
-# define PRI_FILENAME	"key.pem"
-# define PUB_FILENAME	"key_pub.pem"
+#define PRI_FILENAME "key.pem"
+#define PUB_FILENAME "key_pub.pem"
 
 EC_KEY *ec_create(void);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
+EC_KEY *ec_load(char const *folder);
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 int ec_save(EC_KEY *key, char const *folder);
 
